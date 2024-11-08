01f20109116b52f36569af70638c1c9ce1f1a0eb
DevCompleteStudios <jorge.yael333@gmail.com>
2024-11-08 15:51:10 -0600

    Agregando formulario de Login - falta logica y hacer los componentes reutilizables

--- src/app/pages/auth-login/auth-login.component.ts
+++ src/app/pages/auth-login/auth-login.component.ts
@@ -1,13 +1,53 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { merge } from 'rxjs';
+
 
 @Component({
   selector: 'app-auth-login',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule],
   templateUrl: './auth-login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthLoginComponent { }
+export class AuthLoginComponent {
+
+  protected form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  protected emailError = signal('');
+  protected passwordError = signal('');
+  protected hide = signal(true);
+
+
+  constructor(){
+    merge(this.form.statusChanges, this.form.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.detectErrors());
+  }
+
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  detectErrors(){
+    const emailField = this.form.controls.email;
+    const passField = this.form.controls.password;
+
+    if( emailField.hasError('email') ){
+      this.emailError.set('Email is not valid');
+    } else {
+      this.emailError.set('');
+    }
+  }
+
+
+}
